db0161fafb3ac8977bf48f8e11508d02a64914bf
Thura122016 <60479205+Thura122016@users.noreply.github.com>
2020-03-12 01:29:25 +0630

    Update EasyGuard_ATY.py

--- EasyGuard_ATY.py
+++ EasyGuard_ATY.py
@@ -4,9 +4,9 @@ ed = input("1 or 2  : ")
 u = input("Enter email : ")
 p = input("Password : ")
 if ed=="1":
-    pyhton guard.py --user=u --password=p --enable
+    guard.py --user=u --password=p --enable
 elif ed=="2":
-    pyhton guard.py --user=u --password=p --disable
+    guard.py --user=u --password=p --disable
 else:
     print("Error : pls choose 1 or 2.")
 input()
