4dd2b631213ee427eca1a45885e712fa6e7871b1
Thura122016 <60479205+Thura122016@users.noreply.github.com>
2020-03-12 01:35:36 +0630

    Update EasyGuard_ATY.py

--- EasyGuard_ATY.py
+++ EasyGuard_ATY.py
@@ -1,5 +1,5 @@
 import os
-print("Facebook profile_guard enabler/disabler:3\n\nPls choose 1 or 2\n\n1.Enable\n2.Disable\n")
+print("\nFacebook profile_guard enabler/disabler:3\n\nPls choose 1 or 2\n\n1.Enable\n2.Disable\n")
 ed = input("1 or 2  : ")
 u = input("Enter email : ")
 p = input("Password : ")
